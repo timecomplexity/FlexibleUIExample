a27ae2ba160693aeed62c60ce347b3e18977a99c
blastbeatsandcode <alexsilcott@gmail.com>
2019-09-02 09:04:42 -0400

    Removed extra lines from InteractWithCube.cs.

--- Assets/Scripts/InteractWithCube.cs
+++ Assets/Scripts/InteractWithCube.cs
@@ -10,12 +10,6 @@ public class InteractWithCube : MonoBehaviour
         GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
         this.tooltip = (GameObject)Instantiate(Resources.Load("UI/TextBox/TextBox"), canvas.transform, false);
         this.tooltip.GetComponent<FlexibleUITextBox>().textBoxSkinData = (FlexibleUITextBoxData)Resources.Load("Data/TextBoxes/TextBox_Tooltip_BlueBox");
-        Transform trans = canvas.transform;
-        trans.position = new Vector3(
-            trans.position.x,
-            trans.position.y,
-            trans.position.z
-            );
         GameObject closeButton = (GameObject)Instantiate(Resources.Load("UI/Button/Button"), canvas.transform, false);
         closeButton.GetComponent<Button>().onClick.AddListener(() => CloseToolTip(closeButton));
         closeButton.GetComponent<FlexibleUIButton>().TypeOfButton = FlexibleUIButton.ButtonType.CONFIRM;
